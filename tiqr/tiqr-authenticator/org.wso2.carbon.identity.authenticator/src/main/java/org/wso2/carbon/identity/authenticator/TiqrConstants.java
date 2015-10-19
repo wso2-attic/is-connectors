fed4b3178bcc03724017193dd22aaa4b6dd97d18
Shaki <Shaki@Shakis-MacBook-Pro.local>
2015-10-19 15:32:18 +0530

    Adding Tiqr IS authenticator

---
+++ tiqr/tiqr-authenticator/org.wso2.carbon.identity.authenticator/src/main/java/org/wso2/carbon/identity/authenticator/TiqrConstants.java
@@ -0,0 +1,29 @@
+/*
+ *  Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+
+package org.wso2.carbon.identity.authenticator;
+
+public class TiqrConstants {
+    public static final String AUTHENTICATOR_NAME = "Tiqr";
+    public static final String AUTHENTICATOR_FRIENDLY_NAME = "Tiqr";
+    public static final String TIQR_CLIENTIP = "clientIP";
+    public static final String ENROLL_USERID = "UserId";
+    public static final String ENROLL_DISPLAYNAME = "DiaplayName";
+    public static final String TIQR_WAIT_TIME = "waitTime";
+}
