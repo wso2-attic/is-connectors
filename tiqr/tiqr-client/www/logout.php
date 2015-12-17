3129568e597fbaace17394af5ba34f32414f0686
Shaki <Shaki@Shakis-MacBook-Pro.local>
2015-12-17 21:00:39 +0530

    Adding simplesamlphp

--- tiqr/tiqr-client/www/logout.php
+++ tiqr/tiqr-client/www/logout.php
@@ -1,9 +1,24 @@
 <?php
 
-include('../options.php');
+require_once('_include.php');
 
-session_start();
-$tiqr = new Tiqr_Service($options);
-$sid = session_id();
-$tiqr->logout($sid);
-header("Location: /index.php");
+$config = SimpleSAML_Configuration::getInstance();
+
+if (array_key_exists('link_href', $_REQUEST)) {
+    $link = (string) $_REQUEST['link_href'];
+    $link = \SimpleSAML\Utils\HTTP::normalizeURL($link);
+} else {
+    $link = 'index.php';
+}
+
+if (array_key_exists('link_text', $_REQUEST)) {
+    $text = $_REQUEST['link_text'];
+} else {
+    $text = '{logout:default_link_text}';
+}
+
+$t = new SimpleSAML_XHTML_Template($config, 'logout.php');
+$t->data['link'] = $link;
+$t->data['text'] = $text;
+$t->show();
+exit();
