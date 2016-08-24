08d6917f11d9aa6d7799b8bef8ee5b7496a42c54
elilmatha <elilsiva3@gmail.com>
2016-08-24 10:17:44 +0530

    removing comments

--- components/smsotp/smsotp-authenticator/org.wso2.carbon.identity.authenticator/src/main/java/org/wso2/carbon/identity/authenticator/smsotp/OneTimePassword.java
+++ components/smsotp/smsotp-authenticator/org.wso2.carbon.identity.authenticator/src/main/java/org/wso2/carbon/identity/authenticator/smsotp/OneTimePassword.java
@@ -32,8 +32,7 @@ import java.security.SecureRandom;
  */
 public class OneTimePassword {
     private static Log log = LogFactory.getLog(SMSOTPAuthenticator.class);
-    // These are used to calculate the check-sum digits.
-    // 0 1 2 3 4 5 6 7 8 9
+
     private static final int[] doubleDigits = {0, 2, 4, 6, 8, 1, 3, 5, 7, 9};
 
     /**
